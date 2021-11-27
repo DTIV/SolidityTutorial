e3c38a359dd811e2d530c79ffdd50cbe671af414
DTIV <dan_troyer@hotmail.com>
2021-11-26 23:11:19 -0500

    testnet/mainnet

--- 03_Web3.py/deploy.py
+++ 03_Web3.py/deploy.py
@@ -48,44 +48,40 @@ with open("compiled_code.json", "w") as file:
     json.dump(compiled_sol, file)
 
 print("Compiling Contract Finished")
-##################################
-######         NETWORK      ######
-##################################
-
-#get bytecode
-bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"]["bytecode"]["object"]
-
-#get abi
-abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+########################################
+######      LOCAL BLOCKCHAIN      ######
+########################################
 
 #connect to ganache rpc server
-w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
+#w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
+#address = config['ganache_address']
+#network_id = 1337 #ganache network id
+#private_key = config['ganache_pk']
 
-#network id
-chain_id = 1337
+########################################
+######      TESTNET/MAINNET       ######
+########################################
 
-address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
-
-#must add '0x' to private keys in Ganche UI
-#never hardcode private key
+#connect to ethereum blockchains with infura
+w3 = Web3(Web3.HTTPProvider('https://rinkeby.infura.io/v3/0012ec74762c4360b61265c140f6140e'))
 private_key = config['pk']
+address = config['address']
+network_id = 4 #rinkeby network id
+
+##################################
+######         DEPLOY       ######
+##################################
+
+nonce = w3.eth.getTransactionCount(address) # tx count
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"]["bytecode"]["object"]
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+gas_price = w3.eth.gas_price
 
 #create contract
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
-
-#get nonce - tx count
-nonce = w3.eth.getTransactionCount(address)
-
-#get gas price
-gas_price = w3.eth.gas_price
-
-##################################
-######     TRANSACTIONS     ######
-##################################
-
 #build transactions
 # new addition to original tutorial add gasPrice to your transaction
-transaction = SimpleStorage.constructor().buildTransaction({"chainId": chain_id, "gasPrice": gas_price, "from":address, "nonce": nonce})
+transaction = SimpleStorage.constructor().buildTransaction({"chainId": network_id, "gasPrice": gas_price, "from":address, "nonce": nonce})
 
 #sign transaction with private key
 signed_tx = w3.eth.account.sign_transaction(transaction, private_key)
@@ -98,28 +94,28 @@ tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
 tx_reciept = w3.eth.wait_for_transaction_receipt(tx_hash)
 
 print("Contract Deployed!")
+
 ##################################
 ######       CONTRACT       ######
 ##################################
 
 #working with contract needs ABI and address
 simple_storage = w3.eth.contract(address= tx_reciept.contractAddress, abi=abi)
+
 '''
 Two different ways to interact with contract functions: Call and Transact
     Call - simulates making the call and getting a return value (dont make state change)
     Transact - makes state changes
 '''
-#does NOT actually store the value on the contract
+####  CALL  ####  --- does NOT actually store the value
 call_retrieve = simple_storage.functions.retrieve().call()
 call_store = simple_storage.functions.store(15).call()
 
-#will actually store the value set to contract
-#nonce can only be used one per tx, so nonce+1
-
 print("Updating Contract", simple_storage.functions.retrieve().call())
-
+####   TRANSACTIONS   ##### ---   will actually store the value set to contract
+#nonce can only be used one per tx, so nonce+1
 store_transaction = simple_storage.functions.store(15).buildTransaction(
-    {"chainId":chain_id, "from": address, "nonce": nonce+1, "gasPrice": gas_price}
+    {"chainId":network_id, "from": address, "nonce": nonce+1, "gasPrice": gas_price}
 )
 
 sign_store_tx = w3.eth.account.sign_transaction(store_transaction, private_key=private_key)
